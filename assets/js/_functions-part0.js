ebf0c8c8b34c8f68fa6f044ef0c645bb22039073
Heinz K <heka@techcollege.dk>
2020-10-19 13:42:47 +0200

    Initial Commit

---
+++ assets/js/_functions-part0.js
@@ -0,0 +1,13 @@
+
+
+/**
+ * Funktion til at teste for loop med
+ * @param {number} max 
+ */
+function counter(max = 10) {
+    for(let i = 1; i <= max; i++) {
+        console.log(i);
+    }
+}
+
+counter(5);
---
+++ assets/js/_functions-part1.js
@@ -0,0 +1,18 @@
+/**
+ * Funktion til at sætte baggrundsfarve med 
+ * Modtager farvenavn som parameter
+ * @param {string} color 
+ */
+function setColor(color) {
+    document.body.style.backgroundColor = color;    
+}
+
+/**
+ * 
+ * @param {float} myPrice 
+ */
+function getCurrency(myPrice) {
+    return myPrice * 0,7;
+}
+
+/** */
