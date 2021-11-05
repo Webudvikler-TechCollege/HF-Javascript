128d2997aafcfbc65bca33469fd5e45718c23da5
Heinz K <heka@techcollege.dk>
2021-11-05 09:48:44 +0100

    Misc Commit

--- Projects/memory/assets/js/_game.js
+++ Projects/memory/assets/js/_game.js
@@ -1,24 +1,24 @@
 // importerer funktion fra model
 import { GoalModel } from './model.js';
-
+let arrGoalArray = GoalModel();
 //console.log(arrGoalArray);
 
-// Gameboard
+// Var til gameboard object
 const gameboard = document.getElementById('game');
-// Timeboard
+// Var til timer 
 const timeboard = document.getElementById('timer');
-// Antal kort
+// Var til antal kort
 const num_cards = 10;
 // Array til active cards
 const arr_flipped = [];
-// Tæller par
+// Var til matched pairs
 let pairs = 0;
-// Timer Vars
-let miliseconds = '00';
-let seconds = '00';
-let minutes = '00';
+// Vars til timer function
+let miliseconds = 0;
+let seconds = "00";
+let minutes = "00";
+let timer_output = '00:00:00';
 let timer;
-
 // Initialiserer spillet
 initGame(num_cards);
 
@@ -26,8 +26,6 @@ initGame(num_cards);
  * Initialiser spil
  */
 function initGame(num_cards) {
-
-    let arrGoalArray = GoalModel();
     // Sorter array tilfældigt
     arrGoalArray.sort(() => Math.random() - 0.5);
     // Slicer array til num_cards
@@ -68,9 +66,9 @@ function initGame(num_cards) {
 }
 
 function flipCard(divElm) {
-    if(!timer) {
-        setTimer();
-        timer = setInterval(setTimer, 10)
+    if(!miliseconds) {
+        startTimer();
+        timer = setInterval(startTimer, 10);
     }
 
     divElm.classList.add('active');
@@ -78,12 +76,12 @@ function flipCard(divElm) {
     arr_flipped.push(divElm);
 
     if(arr_flipped.length === 2) {
-        //console.log(arr_flipped);
+
         if(arr_flipped[0].innerHTML === arr_flipped[1].innerHTML) {
             pairs++;
             arr_flipped.length = 0;
             if(pairs === num_cards) {
-                gameOver();
+                // Spillet er slut
                 clearInterval(timer);
             }
         } else {
@@ -93,60 +91,25 @@ function flipCard(divElm) {
                 }
                 arr_flipped.length = 0;
             }, 1400);
+
         }
     }
 }
 
-function setTimer() {
+function startTimer() {    
+
     miliseconds++;
     miliseconds = (miliseconds < 10 ? '0' : "") + miliseconds;
 
     if(miliseconds > 99) {
         seconds++;
-        seconds = (seconds < 10 ? '0' : "") + seconds;
+        seconds = (seconds < 10 ? "0" : "") + seconds;
         miliseconds = '00';
-
         if(seconds > 59) {
             minutes++;
-            minutes = (minutes < 10 ? '0' : "") + minutes;
-            seconds = '00';
+            minutes = (minutes < 10 ? "0" : "") + minutes;
+            seconds = 0;
         }
     }
-    timeboard.innerText = `${minutes}:${seconds}:${miliseconds}`;
+    timeboard.innerText = minutes + ":" + seconds + ":" + miliseconds;
 }
-
-function gameOver() {
-    let div = document.createElement('div');
-    div.classList.add('gameover');
-    
-    let h1 = document.createElement('h1');
-    h1.innerHTML = 'Spillet er færdigt';
-    div.prepend(h1);
-
-    let p = document.createElement('p');
-    p.innerHTML = `Din tid er ${minutes}:${seconds}:${miliseconds}`;
-    div.append(p);
-
-    let btn = document.createElement('button');
-    btn.innerText = 'Spil igen';
-    btn.addEventListener('click', () => {
-        reset();
-    })
-    div.append(btn);
-
-    document.body.append(div);
-}
-
-function reset() {
-    document.querySelector('.gameover').remove();
-    gameboard.innerHTML = null;
-    initGame(num_cards);
-    pairs = 0;
-    timer = null;
-    timeboard.innerText = `00:00:00`;
-    miliseconds = '00';
-    seconds = '00';
-    minutes = '00';
-}
-
-
